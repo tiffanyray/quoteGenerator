bcf777406630be13ca895ee21b3905169101100c
tiffanyray <tiffanyraylee@gmail.com>
2020-08-05 14:11:00 -0600

    Added error handling

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom";
 import "./styles/index.sass";
 
 class QuoteBox extends React.Component {
-  getRandomNumber(min, max) {
-    min = Math.ceil(104);
-    max = Math.floor(2568);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  // apiURL = `https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&_=${this.getRandomNumber()}`
-
   constructor(props) {
     super(props);
     this.state = {
@@ -24,45 +16,57 @@ class QuoteBox extends React.Component {
     this.getRandomQuote();
   }
 
+  getRandomNumber(min, max) {
+    min = Math.ceil(10900);
+    max = Math.floor(11000);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   getRandomQuote() {
     fetch(
       `https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&_=${this.getRandomNumber()}`
     )
       .then((response) => {
-        console.log("response", response.status);
-        if (response.status == 200) {
-          console.log("status === 200 true");
-          return response.json();
-        } else {
-          throw error;
-        }
+        if (response.status !== 200) throw error;
+
+        return response.json();
       })
       .then((data) => {
-        console.log("data", data);
-        const howToReplace = (match, string, p1, p2, p3) => {
-          console.log("howToReplace::", match, string);
-          let parser = new DOMParser();
-          console.log("match", match.normalize());
-          return "HERE";
-        };
-
-        let parser = new DOMParser();
-
         this.setState({
           content: data[0].content.rendered,
           title: data[0].title.rendered,
+          error: null,
         });
       })
-      .catch((error) => {
-        console.error(
-          "There was an error with the API. Please try again later.",
-          error
-        );
+      .catch(() => {
+        let errorMessage =
+          "There was an error with the API. Please try again later.";
+        this.setState({
+          error: errorMessage,
+          content: null,
+          title: null,
+        });
+        console.error(errorMessage);
       });
   }
 
   render() {
-    console.log("state content", this.state.content);
+    if (this.state.error) {
+      return (
+        <div id="main-div">
+          <h1>Random Quote</h1>
+          <div id="quote-box">
+            <div
+              id="text"
+              dangerouslySetInnerHTML={{ __html: this.state.error }}
+            />
+            <button id="new-quote" onClick={this.getRandomQuote}>
+              Try Again
+            </button>
+          </div>
+        </div>
+      );
+    }
     return (
       <div id="main-div">
         <h1>Random Quote</h1>
